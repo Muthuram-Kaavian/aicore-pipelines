b1028dae0c16a2cfc9f768d70060fef8887c8b48
Muthuram-Kaavian <muthuram.m@kaaviansys.com>
2025-06-20 15:55:17 +0530

    house in vs toolkit

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
-import joblib
+# from sklearn import datasets
+# from sklearn.model_selection import train_test_split
+# from sklearn.tree import DecisionTreeRegressor
+# import joblib
 
-# Load California housing dataset
-data_house = datasets.fetch_california_housing(data_home='/app/src')
+# # Load California housing dataset
+# data_house = datasets.fetch_california_housing(data_home='/app/src')
+# X = data_house['data']
+# y = data_house['target']
+
+# # Split data
+# train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
+
+# # Train model
+# clf = DecisionTreeRegressor()
+# clf.fit(train_x, train_y)
+
+# # Save trained model
+# joblib.dump(clf, 'inference/model.joblib')
+
+# # Print R2 score
+# test_score = clf.score(test_x, test_y)
+# print(f"Test Data Score: {test_score}")
+
+
+# Load Datasets
+from sklearn import datasets
+data_house = datasets.fetch_california_housing()
 X = data_house['data']
 y = data_house['target']
-
-# Split data
+#
+# Partition into Train and test dataset
+from sklearn.model_selection import train_test_split
 train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
-
-# Train model
+#
+# Init model
+from sklearn.tree import DecisionTreeRegressor
 clf = DecisionTreeRegressor()
+#
+# Train model
 clf.fit(train_x, train_y)
+#
+# Test model
+test_r2_score = clf.score(test_x, test_y)
 
-# Save trained model
-joblib.dump(clf, 'inference/model.joblib')
-
-# Print R2 score
-test_score = clf.score(test_x, test_y)
-print(f"Test Data Score: {test_score}")
+# Output will be available in logs of SAP AI Core.
+# Not the ideal way of storing /reporting metrics in SAP AI Core, but that is not the focus this tutorial
+print(f"Test Data Score {test_r2_score}")
